ba2ec96041b547e921458824cbd313ef9898a0de
Yu Cheng <69800322+Wu22143@users.noreply.github.com>
2022-05-17 14:41:32 +0800

    week12-新增simple_contour , find_contour , bounding_box

--- homework.py
+++ homework.py
@@ -30,18 +30,25 @@ class App():
         self.menu_cvt.add_command(label='to RGB' , command=self.functions.cvttoRGB)
         self.menu_cvt.add_command(label='to HSV' , command=self.functions.cvttoHSV)
         self.menu_cvt.add_command(label='to Gray', command=self.functions.cvttoGray)
+
         self.file_menu.add_command(label='開啟檔案' , command=self.functions.open_files)
         self.file_menu.add_command(label='儲存檔案' , command=self.functions.save_files)
         self.file_menu.add_separator()
         self.file_menu.add_command(label='離開程式', command=window.quit)
+
         self.menu_setting.add_command(label='設定ROI',command=self.functions.ROI)
         self.menu_setting.add_command(label='顯示影像直方圖',command=self.functions.calHist)
         self.menu_setting.add_cascade(label='顯示或改變色彩空間',menu=self.menu_cvt)
+
         self.menu_imageprocess.add_command(label='影像二值化(Thresholding)',command=self.functions.fun_Threshoulding)
         self.menu_imageprocess.add_command(label='直方圖等化(Histogram Equalization)',command=self.functions.fun_EqualizeHist)
         self.menu_imageprocess.add_cascade(label='濾波器(Filter)',menu=self.menu_filter)
         self.menu_imageprocess.add_cascade(label='仿射轉換(Affine Transform',menu=self.menu_affine)
         self.menu_imageprocess.add_command(label='透視變換',command=self.functions.Perspective_Transformation)
+        self.menu_imageprocess.add_command(label='Canny Detector',command=self.functions.canny_detector)
+        self.menu_imageprocess.add_command(label='霍夫轉換',command=self.functions.hough_transform)
+        self.menu_imageprocess.add_command(label='Corner_Harris',command=self.functions.conrer_Harris)
+
         self.menu_filter.add_command(label='中值濾波',command=self.functions.Median_filter)
         self.menu_filter.add_command(label='均值濾波',command=self.functions.averaging_filter)
         self.menu_filter.add_command(label='高斯濾波',command=self.functions.Gaussian_filter)
